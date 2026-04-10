82f91f0e9dfec6f6663c06dc3643392307fb30e9
Robin Reichert <robin@reichert-online.eu>
2026-04-10 12:48:34 +0200

    added hook

--- cresco.sh
+++ cresco.sh
@@ -4,6 +4,11 @@ case "$1" in
   serve_docs)
     docker compose -f docker/docker-compose-docs.yml up
     ;;
+  setup)
+    cp scripts/pre-commit .git/hooks/pre-commit
+    chmod +x .git/hooks/pre-commit
+    echo "Pre-commit hook installed!"
+    ;;
   *)
     echo "Usage: $0 {serve_docs}"
     exit 1

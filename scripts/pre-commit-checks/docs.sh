93e1b851558b123154b0c4a91e4f3a117323fec2
Robin Reichert <62264462+RobinReichert@users.noreply.github.com>
2026-04-10 22:58:34 +0200

    refactor docs config (#21)
    * testing markdownlint config
    
    * testing markdownlint config
    
    * add rule to github workflow
    
    * removed echo
    
    * testing new build config
    
    * moved config into one directory
    
    * testing pre commit checks
    
    * update github workflows
    
    * updated excludes

--- scripts/pre-commit-checks/docs.sh
+++ scripts/pre-commit-checks/docs.sh
@@ -1,6 +1,6 @@
-  echo 'Running markdownlint...' >&2 &&"
-  npx markdownlint 'docs/**/*.md' &&"
-  echo 'Running jeykyll build...' >&2 &&"
-  cd docs &&"
-  bundle exec jekyll build --destination ../../_site --config ghp/_config.yml &&"
-  cd .. &&"
+  echo 'Running markdownlint...' >&2
+  npx markdownlint --config docs/config/.markdownlint.yml 'docs/**/*.md'
+  echo 'Running jeykyll build...' >&2
+  cd docs
+  bundle exec jekyll build --destination ../../_site --config config/_config.yml
+  cd ..

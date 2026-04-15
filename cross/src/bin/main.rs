0838b5816f97e2e3a50f70514130d85bc2107ce0
Robin Reichert <robin@reichert-online.eu>
2026-04-16 01:06:11 +0200

    testing

--- cross/src/bin/main.rs
+++ cross/src/bin/main.rs
@@ -20,7 +20,6 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
 }
 
 extern crate alloc;
-
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();

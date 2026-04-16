e5df7867e16092a67cf70aa1032cd568a8a4b3a3
Robin Reichert <robin@reichert-online.eu>
2026-04-16 12:53:56 +0200

    added -h to cresco.sh

--- cresco.sh
+++ cresco.sh
@@ -3,6 +3,9 @@
 ELF_PATH="build/app_embedded/riscv32imc-unknown-none-elf/release/cross"
 
 case "$1" in
+  -h|--help)
+      show_help()
+    ;;
   serve_docs)
     docker compose -f docker/docker-compose-docs-server.yml run --rm --service-ports docs-server
     ;;
@@ -36,3 +39,41 @@ case "$1" in
     exit 1
     ;;
 esac
+
+show_help() {
+  cat << EOF
+Usage: $0 <command>
+
+Commands:
+
+  serve_docs
+      Start the documentation server using Docker Compose.
+
+  setup
+      Install or update the git pre-commit hook.
+
+  format
+      Format the Rust code using cargo fmt.
+
+  test
+      Run tests for the 'logic' package.
+
+  build_app_embedded
+      Build the embedded application in release mode.
+
+  flash
+      Flash the compiled ELF to the ESP32-C3 device.
+
+  monitor
+      Open a serial monitor with defmt logs for the ESP32-C3.
+
+  -h, --help
+      Show this help message.
+
+Examples:
+
+  $0 setup
+  $0 build_app_embedded
+  $0 flash
+EOF
+}

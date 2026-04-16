4386ca940cbc0b56db69f3014a55cc156591308c
Robin Reichert <62264462+RobinReichert@users.noreply.github.com>
2026-04-16 13:09:29 +0200

    fixed script (#46)

--- cresco.sh
+++ cresco.sh
@@ -2,44 +2,6 @@
 
 ELF_PATH="build/app_embedded/riscv32imc-unknown-none-elf/release/cross"
 
-case "$1" in
-  -h|--help)
-      show_help()
-    ;;
-  serve_docs)
-    docker compose -f docker/docker-compose-docs-server.yml run --rm --service-ports docs-server
-    ;;
-  setup)
-    if cmp -s scripts/pre-commit .git/hooks/pre-commit; then
-      echo "Pre-commit hook already up to date"
-    else
-      cp scripts/pre-commit .git/hooks/pre-commit
-      chmod +x .git/hooks/pre-commit
-      echo "Pre-commit hook installed/updated!"
-    fi
-    ;;
-  format)
-      cargo fmt
-    ;;
-  test)
-      cargo test -p logic
-    ;;
-  build_app_embedded)
-      cd cross
-      cargo build --release
-    ;;
-  flash)
-      espflash flash $ELF_PATH
-    ;;
-  monitor)
-      espflash monitor --chip esp32c3 --log-format defmt --elf $ELF_PATH
-    ;;
-  *)
-    echo "Usage: $0 {serve_docs}"
-    exit 1
-    ;;
-esac
-
 show_help() {
   cat << EOF
 Usage: $0 <command>
@@ -77,3 +39,42 @@ Examples:
   $0 flash
 EOF
 }
+
+case "$1" in
+  -h|--help)
+      show_help
+    ;;
+  serve_docs)
+    docker compose -f docker/docker-compose-docs-server.yml run --rm --service-ports docs-server
+    ;;
+  setup)
+    if cmp -s scripts/pre-commit .git/hooks/pre-commit; then
+      echo "Pre-commit hook already up to date"
+    else
+      cp scripts/pre-commit .git/hooks/pre-commit
+      chmod +x .git/hooks/pre-commit
+      echo "Pre-commit hook installed/updated!"
+    fi
+    ;;
+  format)
+      cargo fmt
+    ;;
+  test)
+      cargo test -p logic
+    ;;
+  build_app_embedded)
+      cd cross
+      cargo build --release
+    ;;
+  flash)
+      espflash flash $ELF_PATH
+    ;;
+  monitor)
+      espflash monitor --chip esp32c3 --log-format defmt --elf $ELF_PATH
+    ;;
+  *)
+    echo "Usage: $0 {serve_docs}"
+    exit 1
+    ;;
+esac
+

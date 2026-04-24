bc2de847f16035c0a513731de746b62c150d366d
Robin Reichert <robin@reichert-online.eu>
2026-04-24 23:05:52 +0200

    added drawio support for local preview and hopefully for pages

--- cresco.sh
+++ cresco.sh
@@ -41,40 +41,42 @@ EOF
 }
 
 case "$1" in
-  -h|--help)
-      show_help
-    ;;
-  serve_docs)
-    docker compose -f docker/docker-compose-docs-server.yml run --rm --service-ports docs-server
-    ;;
-  setup)
-    if cmp -s scripts/pre-commit .git/hooks/pre-commit; then
-      echo "Pre-commit hook already up to date"
-    else
-      cp scripts/pre-commit .git/hooks/pre-commit
-      chmod +x .git/hooks/pre-commit
-      echo "Pre-commit hook installed/updated!"
-    fi
-    ;;
-  format)
-      cargo fmt
-    ;;
-  test)
-      cargo test -p logic
-    ;;
-  build_app_embedded)
-      cd cross
-      cargo build --release
-    ;;
-  flash)
-      espflash flash $ELF_PATH
-    ;;
-  monitor)
-      espflash monitor --chip esp32c3 --log-format defmt --elf $ELF_PATH
-    ;;
-  *)
-    echo "Usage: $0 {serve_docs}"
-    exit 1
-    ;;
+    -h|--help)
+        show_help
+        ;;
+    serve_docs)
+        docker compose -f docker/docker-compose-docs-server.yml run --rm drawio-export
+        docker compose -f docker/docker-compose-docs-server.yml run --rm --service-ports docs-server
+        ;;
+    setup)
+        if cmp -s scripts/pre-commit .git/hooks/pre-commit; then
+            echo "Pre-commit hook already up to date"
+        else
+            cp scripts/pre-commit .git/hooks/pre-commit
+            chmod +x .git/hooks/pre-commit
+            echo "Pre-commit hook installed/updated!"
+        fi
+        docker pull rlespinasse/drawio-export
+        ;;
+    format)
+        cargo fmt
+        ;;
+    test)
+        cargo test -p logic
+        ;;
+    build_app_embedded)
+        cd cross
+        cargo build --release
+        ;;
+    flash)
+        espflash flash $ELF_PATH
+        ;;
+    monitor)
+        espflash monitor --chip esp32c3 --log-format defmt --elf $ELF_PATH
+        ;;
+    *)
+        echo "Usage: $0 {serve_docs}"
+        exit 1
+        ;;
 esac
 
